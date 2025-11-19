576ee4feaa59dc0f5f5291e5dbf05d9a87a6fcb7
Filip Dolník <github@filipdolnik.com>
2025-11-19 12:28:23 +0100

    Switch to Kotlin 2.3.0-RC.

--- dev-support/build.gradle.kts
+++ dev-support/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 //     kotlin("multiplatform") version "2.2.0" apply false
 //     kotlin("multiplatform") version "2.2.20" apply false
 //     kotlin("multiplatform") version "2.2.21" apply false
-    kotlin("multiplatform") version "2.3.0-Beta2" apply false
+    kotlin("multiplatform") version "2.3.0-RC" apply false
 }
 
 buildscript {
